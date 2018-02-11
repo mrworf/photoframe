872d412cca4e84d5afdae951d82433c0df77132e
Henric Andersson <git@sensenet.nu>
2018-02-10 22:24:41 -0800

    Extracted slideshow to class

--- frame.py
+++ frame.py
@@ -16,19 +16,13 @@ import threading
 from modules.remember import remember
 from modules.shutdown import shutdown
 from modules.timekeeper import timekeeper
-from modules.colormatch import colormatch
 from modules.settings import settings
 from modules.helper import helper
 from modules.display import display
 from modules.oauth import OAuth
+from modules.slideshow import slideshow
 
-# From https://stackoverflow.com/questions/11269575/how-to-hide-output-of-subprocess-in-python-2-7
-try:
-    from subprocess import DEVNULL
-except ImportError:
-    import os
-    DEVNULL = open(os.devnull, 'wb')
-###
+void = open(os.devnull, 'wb')
 
 import requests
 from requests_oauthlib import OAuth2Session
@@ -41,43 +35,6 @@ logging.getLogger('oauthlib').setLevel(logging.ERROR)
 logging.getLogger('urllib3').setLevel(logging.ERROR)
 
 app = Flask(__name__, static_url_path='')
-
-def pick_image(images, memory):
-	ext = ['jpg','png','dng','jpeg','gif','bmp']
-	count = len(images['feed']['entry'])
-	tries = 5
-
-	while tries > 0:
-		i = random.SystemRandom().randint(0,count-1)
-		if not memory.seen(i):
-			memory.saw(i)
-			entry = images['feed']['entry'][i]
-			# Make sure we don't get a video, unsupported for now (gif is usually bad too)
-			if 'image' in entry['content']['type'] and not 'gif' in entry['content']['type']:
-				break
-			else:
-				logging.warning('Unsupported media: %s' % entry['content']['type'])
-		else:
-			logging.debug('Already seen index %d' % i)
-		tries -= 1
-
-	if tries == 0:
-		logging.error('Failed to find any image, abort')
-		return ('', '', '', 0)
-
-	title = entry['title']['$t']
-	parts = title.lower().split('.')
-	if len(parts) > 0 and parts[len(parts)-1] in ext:
-		# Title isn't title, it's a filename
-		title = ""
-	uri = entry['content']['src']
-	timestamp = datetime.datetime.fromtimestamp((float)(entry['gphoto$timestamp']['$t']) / 1000)
-	mime = entry['content']['type']
-
-	# Due to google's unwillingness to return what I own, we need to hack the URI
-	uri = uri.replace('/s1600/', '/s%s/' % settings.getUser('width'), 1)
-
-	return (uri, mime, title, timestamp)
 
 @app.route('/setting', methods=['GET'], defaults={'key':None,'value':None})
 @app.route('/setting/<key>', methods=['GET'], defaults={'value':None})
@@ -157,12 +114,12 @@ def cfg_reset():
 
 @app.route('/reboot')
 def cfg_reboot():
-	subprocess.call(['/sbin/reboot'], stderr=DEVNULL);
+	subprocess.call(['/sbin/reboot'], stderr=void);
 	return jsonify({'reboot' : True})
 
 @app.route('/shutdown')
 def cfg_shutdown():
-	subprocess.call(['/sbin/poweroff'], stderr=DEVNULL);
+	subprocess.call(['/sbin/poweroff'], stderr=void);
 	return jsonify({'shutdown': True})
 
 @app.route('/')
@@ -171,16 +128,10 @@ def web_main():
 
 @app.route("/link")
 def oauth_step1():
-	""" Step 1: Get authorization
-	"""
 	return redirect(oauth.initiate())
-
-# Step 2: Google stuff, essentially user consents to allowing us access
 
 @app.route("/callback", methods=["GET"])
 def oauth_step3():
-	""" Step 3: Get the token
-	"""
 	oauth.complete(request.url)
 	return redirect(url_for('.complete'))
 
@@ -188,125 +139,6 @@ def oauth_step3():
 def complete():
 	slideshow(True)
 	return redirect('/')
-
-def get_images():
-	keyword = settings.getKeyword()
-
-	# Create filename from keyword
-	filename = hashlib.new('md5')
-	filename.update(keyword)
-	filename = filename.hexdigest() + ".json"
-	filename = os.path.join(settings.get('tempfolder'), filename)
-
-	if os.path.exists(filename): # Check age!
-		age = math.floor( (time.time() - os.path.getctime(filename)) / 3600)
-		if age >= settings.getUser('refresh-content'):
-			logging.debug('File too old, %dh > %dh, refreshing' % (age, settings.getUser('refresh-content')))
-			os.remove(filename)
-			# Make sure we don't remember since we're refreshing
-			memory = remember(filename, 0)
-			memory.forget()
-
-	if not os.path.exists(filename):
-		# Request albums
-		# Picasa limits all results to the first 1000, so get them
-		params = {
-			'kind' : 'photo',
-			'start-index' : 1,
-			'max-results' : 1000,
-			'alt' : 'json',
-			'access' : 'all',
-			'imgmax' : '1600u', # We will replace this with width of framebuffer in pick_image
-			# This is where we get cute, we pick from a list of keywords
-			'fields' : 'entry(title,content,gphoto:timestamp)' # No unnecessary stuff
-		}
-		if keyword != "":
-			params['q'] = keyword
-		url = 'https://picasaweb.google.com/data/feed/api/user/default'
-		logging.debug('Downloading image list for %s...' % keyword)
-		data = oauth.request(url, params=params)
-		with open(filename, 'w') as f:
-			f.write(data.content)
-	images = None
-	with open(filename) as f:
-		images = json.load(f)
-	logging.debug('Loaded %d images into list' % len(images['feed']['entry']))
-	return images, filename
-
-def download_image(uri, dest):
-	logging.debug('Downloading %s...' % uri)
-	filename, ext = os.path.splitext(dest)
-	temp = "%s-org%s" % (filename, ext)
-	if oauth.request(uri, destination=temp):
-		if colormatch.hasSensor():
-			if not colormatch.adjust(temp, dest):
-				logging.warning('Unable to adjust image to colormatch, using original')
-				os.rename(temp, dest)
-		else:
-			logging.info('No color temperature info yet')
-			os.rename(temp, dest)
-		return True
-	else:
-		return False
-
-slideshow_thread = None
-
-def slideshow(blank=False):
-	global slideshow_thread
-
-	if blank:
-		display.clear()
-
-	def imageloop():
-		global slideshow_thread
-		time.sleep(1) # Ugly, but works... allows server to get going
-
-		# Make sure we have OAuth2.0 ready
-		while True:
-			if settings.get('oauth_token') is None:
-				display.message('Please link photoalbum\n\nSurf to http://%s:7777/' % settings.get('local-ip'))
-				logging.info('You need to link your photoalbum first')
-				break
-
-			imgs = cache = memory = None
-			tries = 50
-			while tries > 0:
-				imgs, cache = get_images()
-				if not imgs:
-					tries -= 1
-					continue
-
-				memory = remember(cache, len(imgs['feed']['entry']))
-				if memory.seenAll():
-					logging.debug('Seen all images, try again')
-					tries -= 1
-					continue
-
-				# Now, lets make sure we didn't see this before
-				uri, mime, title, ts = pick_image(imgs, memory)
-				if uri == '':
-					tries -= 1
-					continue
-
-				filename = os.path.join(settings.get('tempfolder'), 'image.%s' % helper.getExtension(mime))
-				if download_image(uri, filename):
-					display.image(filename)
-					break
-				else:
-					tries -= 1
-
-			if tries == 0:
-				display.message("Unable to download ANY images\nCheck that you have photos\nand queries aren't too strict")
-			time.sleep(settings.getUser('interval'))
-			if int(time.strftime('%H')) >= settings.getUser('display-off'):
-				logging.debug("It's after hours, exit quietly")
-				break
-		slideshow_thread = None
-
-	if slideshow_thread is None and oauth is not None:
-		slideshow_thread = threading.Thread(target=imageloop)
-		slideshow_thread.daemon = True
-		slideshow_thread.start()
 
 settings = settings()
 settings.load()	
@@ -347,16 +179,15 @@ else:
 
 # Prep random
 random.seed(long(time.clock()))
-
-colormatch = colormatch(settings.get('colortemp-script'), 3500)
-timekeeper = timekeeper(settings.getUser('display-on'), settings.getUser('display-off'), display.enable, slideshow)
+slideshow = slideshow(display, settings, oauth)
+timekeeper = timekeeper(settings.getUser('display-on'), settings.getUser('display-off'), display.enable, slideshow.start)
 powermanagement = shutdown()
 
 if __name__ == "__main__":
 	# This allows us to use a plain HTTP callback
 	os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 	app.secret_key = os.urandom(24)
-	slideshow()
+	slideshow.start()
 	app.run(debug=False, port=7777, host='0.0.0.0' )
 
 sys.exit(0)
---
+++ modules/slideshow.py
@@ -0,0 +1,168 @@
+import threading
+import logging
+import os
+import random
+import datetime
+import hashlib
+import time
+import json
+import math
+
+from modules.remember import remember
+from modules.helper import helper
+from modules.colormatch import colormatch
+
+class slideshow:
+	def __init__(self, display, settings, oauth):
+		self.thread = None
+		self.display = display
+		self.settings = settings
+		self.oauth = oauth
+		self.colormatch = colormatch(self.settings.get('colortemp-script'), 3500)
+
+	def start(self, blank=False):
+		if blank:
+			self.display.clear()
+
+		if self.settings.get('oauth_token') is None:
+			self.display.message('Please link photoalbum\n\nSurf to http://%s:7777/' % self.settings.get('local-ip'))
+			logging.info('You need to link your photoalbum first')
+		elif self.thread is None:
+			self.thread = threading.Thread(target=self.presentation)
+			self.thread.daemon = True
+			self.thread.start()
+
+	def presentation(self):
+		while True:
+			imgs = cache = memory = None
+			tries = 50
+			while tries > 0:
+				imgs, cache = self.getImages()
+				if not imgs:
+					tries -= 1
+					continue
+
+				memory = remember(cache, len(imgs['feed']['entry']))
+				if memory.seenAll():
+					logging.debug('Seen all images, try again')
+					tries -= 1
+					continue
+
+				# Now, lets make sure we didn't see this before
+				uri, mime, title, ts = self.pickImage(imgs, memory)
+				if uri == '':
+					tries -= 1
+					continue
+
+				filename = os.path.join(self.settings.get('tempfolder'), 'image.%s' % helper.getExtension(mime))
+				if self.downloadImage(uri, filename):
+					self.display.image(filename)
+					break
+				else:
+					tries -= 1
+
+			if tries == 0:
+				self.display.message("Unable to download ANY images\nCheck that you have photos\nand queries aren't too strict")
+			time.sleep(self.settings.getUser('interval'))
+			if int(time.strftime('%H')) >= self.settings.getUser('display-off'):
+				logging.debug("It's after hours, exit quietly")
+				break
+		self.thread = None
+
+	def pickImage(self, images, memory):
+		ext = ['jpg','png','dng','jpeg','gif','bmp']
+		count = len(images['feed']['entry'])
+		tries = 5
+
+		while tries > 0:
+			i = random.SystemRandom().randint(0,count-1)
+			if not memory.seen(i):
+				memory.saw(i)
+				entry = images['feed']['entry'][i]
+				# Make sure we don't get a video, unsupported for now (gif is usually bad too)
+				if 'image' in entry['content']['type'] and not 'gif' in entry['content']['type']:
+					break
+				else:
+					logging.warning('Unsupported media: %s' % entry['content']['type'])
+			else:
+				logging.debug('Already seen index %d' % i)
+			tries -= 1
+
+		if tries == 0:
+			logging.error('Failed to find any image, abort')
+			return ('', '', '', 0)
+
+		title = entry['title']['$t']
+		parts = title.lower().split('.')
+		if len(parts) > 0 and parts[len(parts)-1] in ext:
+			# Title isn't title, it's a filename
+			title = ""
+		uri = entry['content']['src']
+		timestamp = datetime.datetime.fromtimestamp((float)(entry['gphoto$timestamp']['$t']) / 1000)
+		mime = entry['content']['type']
+
+		# Due to google's unwillingness to return what I own, we need to hack the URI
+		uri = uri.replace('/s1600/', '/s%s/' % self.settings.getUser('width'), 1)
+
+		return (uri, mime, title, timestamp)
+
+	def getImages(self):
+		keyword = self.settings.getKeyword()
+
+		# Create filename from keyword
+		filename = hashlib.new('md5')
+		filename.update(keyword)
+		filename = filename.hexdigest() + ".json"
+		filename = os.path.join(self.settings.get('tempfolder'), filename)
+
+		if os.path.exists(filename): # Check age!
+			age = math.floor( (time.time() - os.path.getctime(filename)) / 3600)
+			if age >= self.settings.getUser('refresh-content'):
+				logging.debug('File too old, %dh > %dh, refreshing' % (age, self.settings.getUser('refresh-content')))
+				os.remove(filename)
+				# Make sure we don't remember since we're refreshing
+				memory = remember(filename, 0)
+				memory.forget()
+
+		if not os.path.exists(filename):
+			# Request albums
+			# Picasa limits all results to the first 1000, so get them
+			params = {
+				'kind' : 'photo',
+				'start-index' : 1,
+				'max-results' : 1000,
+				'alt' : 'json',
+				'access' : 'all',
+				'imgmax' : '1600u', # We will replace this with width of framebuffer in pick_image
+				# This is where we get cute, we pick from a list of keywords
+				'fields' : 'entry(title,content,gphoto:timestamp)' # No unnecessary stuff
+			}
+			if keyword != "":
+				params['q'] = keyword
+			url = 'https://picasaweb.google.com/data/feed/api/user/default'
+			logging.debug('Downloading image list for %s...' % keyword)
+			data = self.oauth.request(url, params=params)
+			with open(filename, 'w') as f:
+				f.write(data.content)
+		images = None
+		with open(filename) as f:
+			images = json.load(f)
+		logging.debug('Loaded %d images into list' % len(images['feed']['entry']))
+		return images, filename
+
+	def downloadImage(self, uri, dest):
+		logging.debug('Downloading %s...' % uri)
+		filename, ext = os.path.splitext(dest)
+		temp = "%s-org%s" % (filename, ext)
+		if self.oauth.request(uri, destination=temp):
+			if self.colormatch.hasSensor():
+				if not self.colormatch.adjust(temp, dest):
+					logging.warning('Unable to adjust image to colormatch, using original')
+					os.rename(temp, dest)
+			else:
+				logging.info('No color temperature info yet')
+				os.rename(temp, dest)
+			return True
+		else:
+			return False
+
