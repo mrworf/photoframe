94e2ff036ac345855b17f65385a5059d7b0706a3
Andreas Niemöller <andy@AndreasllersMBP.fritz.box>
2018-12-08 21:40:07 +0100

    # another mistake

--- modules/slideshow.py
+++ modules/slideshow.py
@@ -218,14 +218,14 @@ class slideshow:
       albumid = None
       for i in range(len(data['albums'])):
         if data['albums'][i]['title'] == keyword:
-          albumid = data['albums'][i][id]
+          albumid = data['albums'][i]['id']
       
       if albumid is None:
         url = 'https://photoslibrary.googleapis.com/v1/sharedAlbums'
         data = self.oauth.request(url).json()
         for i in range(len(data['sharedAlbums'])):
           if data['sharedAlbums'][i]['title'] == keyword:
-            albumid = data['sharedAlbums'][i][id]
+            albumid = data['sharedAlbums'][i]['id']
 
       # fallback to all pictures if album not available
       if albumid is not None:
