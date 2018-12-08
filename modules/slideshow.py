625d9d0d05cecd15e7537206a2d4ea9652dcd6ee
Andreas Niemöller <andy@AndreasllersMBP.fritz.box>
2018-12-08 21:37:08 +0100

    # search in albums and shared albums

--- modules/slideshow.py
+++ modules/slideshow.py
@@ -220,6 +220,13 @@ class slideshow:
         if data['albums'][i]['title'] == keyword:
           albumid = data['albums'][i][id]
       
+      if albumid is None:
+        url = 'https://photoslibrary.googleapis.com/v1/sharedAlbums'
+        data = self.oauth.request(url).json()
+        for i in range(len(data['sharedAlbums'])):
+        if data['sharedAlbums'][i]['title'] == keyword:
+          albumid = data['sharedAlbums'][i][id]
+
       # fallback to all pictures if album not available
       if albumid is not None:
         logging.debug('Got album: %s' % keyword)
