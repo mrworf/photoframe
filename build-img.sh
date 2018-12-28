034b5be16edaad925a369e00386ed6ceb5ce0b22
Henric Andersson <henric@sensenet.nu>
2018-12-27 21:49:58 -0800

    Working on travis

--- build-img.sh
+++ build-img.sh
@@ -3,9 +3,10 @@
 export PHOTOFRAME_SRC="$(pwd)/*"
 
 if [ -d pi-gen ]; then
-  rm -rf pi-gen
+  sudo rm -rf pi-gen
 fi
 
 git clone https://github.com/mrworf/pi-gen.git
 cd pi-gen
+echo >config.local "PHOTOFRAME_SRC=\"${PHOTOFRAME_SRC}\""
 sudo ./build.sh
