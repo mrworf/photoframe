7aa0a426c7d9b4efdd39dd4f4eb02b5b58a1fefd
Leo Gaube <gaubeleo@gmail.com>
2019-06-08 07:20:29 +0200

    USB-Service added (#85)
    * USB-Service added
    * explain State added + updateState adapted
    * multi-usb support

---
+++ services/svc_usb.py
@@ -0,0 +1,279 @@
+# This file is part of photoframe (https://github.com/mrworf/photoframe).
+#
+# photoframe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# photoframe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with photoframe.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from base import BaseService
+import random
+import subprocess
+import os
+import logging
+
+from modules.helper import helper
+
+
+class USB_Photos(BaseService):
+  SERVICE_NAME = 'USB-Photos'
+  SERVICE_ID = 4
+
+  SUBSTATE_NOT_CONNECTED = 404
+
+  INDEX = 0
+
+  def __init__(self, configDir, id, name):
+    BaseService.__init__(self, configDir, id, name, needConfig=False, needOAuth=False)
+
+  def preSetup(self):
+    USB_Photos.INDEX += 1
+    self.usbDir = "/mnt/usb%d" % USB_Photos.INDEX
+    self.baseDir = os.path.join(self.usbDir, "photoframe")
+
+    self.device = None
+    if not os.path.exists(self.baseDir):
+      self.mountStorageDevice()
+    elif len(os.listdir(self.baseDir)) == 0:
+      self.unmountBaseDir()
+      self.mountStorageDevice()
+    else:
+      self.checkForInvalidKeywords()
+      for device, mountPath in self.detectAllStorageDevices(onlyMounted=True):
+        if mountPath == self.usbDir:
+          self.device = device
+          logging.info("USB-Service has detected device '%s'" % self.device)
+          break
+      if self.device is None:
+        # Service should still be working fine
+        logging.warning("Unable to determine which storage device is mounted to '%s'" % self.usbDir)
+
+  def helpKeywords(self):
+    return "Place photo albums in /photoframe/{album_name} on your usb-device.\nUse the {album_name} as keyword (CasE-seNsitiVe!).\nIf you want to display all albums simply write 'ALLALBUMS' as keyword.\nAlternatively, place images directly inside the '/photoframe/' directory. "
+
+  def validateKeywords(self, keyword):
+    if keyword != 'ALLALBUMS' and keyword != '_PHOTOFRAME_':
+      if keyword not in self.getAllAlbumNames():
+        return {'error': 'No such album "%s"' % keyword, 'keywords': keyword}
+
+    return BaseService.validateKeywords(self, keyword)
+
+  def getKeywords(self):
+    if not os.path.exists(self.baseDir):
+      return []
+
+    keywords = list(self._STATE['_KEYWORDS'])
+    if "ALLALBUMS" in keywords:
+      # No, you can't have an album called /photoframe/ALLALBUMS ...
+      keywords.remove("ALLALBUMS")
+      albums = self.getAllAlbumNames()
+      keywords.extend(filter(lambda a: a not in keywords, albums))
+
+      if "ALLALBUMS" in albums:
+        logging.error("You should not have a album called 'ALLALBUMS'!")
+
+    if len(keywords) == 0 and len(self.getBaseDirImages()) != 0 and "_PHOTOFRAME_" not in keywords:
+      keywords.append("_PHOTOFRAME_")
+      # _PHOTOFRAME_ can be manually deleted via web interface if other keywords are specified!
+
+    self._STATE['_KEYWORDS'] = keywords
+    self.saveState()
+    return keywords
+
+  def checkForInvalidKeywords(self):
+    index = len(self._STATE['_KEYWORDS'])-1
+    for keyword in reversed(self._STATE['_KEYWORDS']):
+      if keyword == "_PHOTOFRAME_":
+        if len(self.getBaseDirImages()) == 0:
+          logging.debug("USB-Service: removing keyword '%s' because there are no images directly inside the basedir!" % keyword)
+          self.removeKeywords(index)
+      elif keyword not in self.getAllAlbumNames():
+        logging.info("USB-Service: removing invalid keyword: %s" % keyword)
+        self.removeKeywords(index)
+      index -= 1
+    self.saveState()
+
+  def updateState(self):
+    self.subState = None
+    if not os.path.exists(self.baseDir):
+      if not self.mountStorageDevice():
+        self._CURRENT_STATE = BaseService.STATE_NO_IMAGES
+        self.subState = USB_Photos.SUBSTATE_NOT_CONNECTED
+        return self._CURRENT_STATE
+    if len(self.getAllAlbumNames()) == 0 and len(self.getBaseDirImages()) == 0:
+      self._CURRENT_STATE = BaseService.STATE_NO_IMAGES
+      return self._CURRENT_STATE
+
+    return BaseService.updateState(self)
+
+  def explainState(self):
+    if self._CURRENT_STATE == BaseService.STATE_NO_IMAGES:
+      if self.subState == USB_Photos.SUBSTATE_NOT_CONNECTED:
+        return "No storage device (e.g. USB-stick) has been detected!"
+      else:
+        return "Place images and/or albums inside a '/photoframe'-directory on your storage device!"
+    return None
+
+  def getMessages(self):
+    # display a message indicating which storage device is being used or an error messing if no suitable storage device could be found
+    if os.path.exists(self.baseDir):
+      msgs = [
+          {
+              'level': 'SUCCESS',
+              'message': 'Storage device "%s" is mounted to "%s"' % (self.device if self.device is not None else "unknown", self.usbDir),
+              'link': None
+          }
+      ]
+      msgs.extend(BaseService.getMessages(self))
+    else:
+      msgs = [
+          {
+              'level': 'ERROR',
+              'message': 'No storage device could be found that contains the "/photoframe/"-directory! Try to reboot or manually mount the desired storage device to "%s"' % self.usbDir,
+              'link': None
+          }
+      ]
+    return msgs
+
+  def detectAllStorageDevices(self, onlyMounted=False, onlyUnmounted=False, reverse=False):
+    storageDevices = []
+    try:
+      fdisk = subprocess.Popen(["lsblk", "-p", "--noheadings", "--raw", "-o", "NAME,MOUNTPOINT"], stdout=subprocess.PIPE)
+      grep = subprocess.Popen(["grep", "/dev/sd.1"], stdin=fdisk.stdout, stdout=subprocess.PIPE)
+      fdisk.stdout.close()
+      output = grep.communicate()[0].strip()
+      if output != "":
+        devices = [d.strip() for d in output.split("\n") if d.strip() != ""]
+        for sd, mountPath in map(lambda d: d.split(" ", 1) if d.find(" ") != -1 else (d, None), devices):
+          if onlyMounted and mountPath is not None:
+            storageDevices.append((sd, mountPath))
+          elif onlyUnmounted:
+            if mountPath is not None:
+              logging.debug("'%s' is already mounted to '%s'" % (sd, mountPath))
+            else:
+              storageDevices.append(sd)
+    except subprocess.CalledProcessError as e:
+      logging.exception('USB-Service: unable to detect storage devices!')
+      logging.error('Output: %s' % repr(e.output))
+
+    # reverse list, because last plugged in storage device is probably the one we are looking for
+    if reverse:
+      storageDevices.reverse()
+    return storageDevices
+
+  def mountStorageDevice(self, storageDevices=None):
+    if not os.path.exists(self.usbDir):
+      cmd = ["sudo", "mkdir", self.usbDir]
+      try:
+        subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+      except subprocess.CalledProcessError as e:
+        logging.exception('Unable to create directory: %s' % cmd[-1])
+        logging.error('Output: %s' % repr(e.output))
+
+    if storageDevices is None:
+      storageDevices = self.detectAllStorageDevices(onlyUnmounted=True, reverse=True)
+    elif isinstance(storageDevices, basestring):
+      storageDevices = list(storageDevices)
+
+    # unplugging/replugging usb-stick causes system to detect it as a new storage device!
+    for device in storageDevices:
+      cmd = ["sudo", "mount", device, self.usbDir]
+      try:
+        subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        logging.info("USB-device '%s' successfully mounted to '%s'!" % (cmd[-2], cmd[-1]))
+        if os.path.exists(self.baseDir):
+          self.device = device
+          self.checkForInvalidKeywords()
+          return True
+      except subprocess.CalledProcessError as e:
+        logging.warning('Unable to mount storage device "%s" to "%s"!' % (device, self.usbDir))
+        logging.warning('Output: %s' % repr(e.output))
+
+    logging.debug("unable to mount any storage device %s to '%s'" % (storageDevices, self.usbDir))
+    return False
+
+  def unmountBaseDir(self):
+    cmd = ["sudo", "umount", self.usbDir]
+    try:
+      subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+      logging.debug("unable to UNMOUNT '%s'" % self.usbDir)
+
+  # All images directly inside '/photoframe' directory will be displayed without any keywords
+  def getBaseDirImages(self):
+    return filter(lambda x: os.path.isfile(os.path.join(self.baseDir, x)), os.listdir(self.baseDir))
+
+  def getAllAlbumNames(self):
+    return filter(lambda x: os.path.isdir(os.path.join(self.baseDir, x)), os.listdir(self.baseDir))
+
+  def selectImageFromAlbum(self, destinationDir, supportedMimeTypes, displaySize, randomize):
+    result = BaseService.selectImageFromAlbum(self, destinationDir, supportedMimeTypes, displaySize, randomize)
+    if result is not None:
+      return result
+
+    if os.path.exists(self.usbDir):
+      return {'id': None, 'mimetype': None, 'error': 'No images could be found on storage device "%s"!\n\nPlease place albums inside /photoframe/{album_name} directory and add each {album_name} as keyword.\n\nAlternatively, put images directly inside the "/photoframe/"-directory on your storage device.' % self.device}
+    else:
+      return {'id': None, 'mimetype': None, 'error': 'No external storage device detected! Please connect a USB-stick!\n\n Place albums inside /photoframe/{album_name} directory and add each {album_name} as keyword.\n\nAlternatively, put images directly inside the "/photoframe/"-directory on your storage device.'}
+
+  def getImagesFor(self, keyword):
+    if not os.path.isdir(self.baseDir):
+      return []
+    images = []
+    if keyword == "_PHOTOFRAME_":
+      files = self.getBaseDirImages()
+      images = self.getAlbumInfo(self.baseDir, files)
+    else:
+      if os.path.isdir(os.path.join(self.baseDir, keyword)):
+        files = os.listdir(os.path.join(self.baseDir, keyword))
+        images = self.getAlbumInfo(os.path.join(self.baseDir, keyword), files)
+      else:
+        logging.warning("The album '%s' does not exist. Did you unplug the storage device associated with '%s'?!" % (os.path.join(self.baseDir, keyword), self.device))
+    return images
+
+  def getAlbumInfo(self, path, files):
+    images = []
+    for filename in files:
+      fullFilename = os.path.join(path, filename)
+      images.append({
+          "id": self.hashString(fullFilename),
+          "url": fullFilename,
+          "source": fullFilename,
+          "mimetype": helper.getMimeType(fullFilename),
+          "size": helper.getImageSize(fullFilename),
+          "filename": filename
+      })
+    return images
+
+  def addUrlParams(self, url, recommendedSize, displaySize):
+    if recommendedSize is None:
+      return url
+    return "%s||%d||%d" % (url, recommendedSize["width"], recommendedSize["height"])
+
+  def requestUrl(self, url, destination=None, params=None, data=None, usePost=False):
+    # pretend to download the file (for compatability with 'selectImageFromAlbum' of baseService)
+    # instead just cache a scaled version of the file and return {status: 200}
+    if url.count("||") == 2:
+      filename, width, height = url.split("||", 2)
+      recSize = {"width": width, "height": height}
+    else:
+      filename = url
+      recSize = None
+
+    if destination is None or not os.path.isfile(filename):
+      return {"status": 400}
+    if recSize is not None:
+      if helper.scaleImage(filename, destination, recSize):
+        return {"status": 200}
+    else:
+      if helper.copyFile(filename, destination):
+        return {"status": 200}
+    return {"status": 418}
