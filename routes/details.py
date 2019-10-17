2c22651d911a6a86965c7c2a3604ba4ed7f1660f
Henric Andersson <ha@sensenet.nu>
2019-10-16 20:56:12 -0700

    Spurious

--- routes/details.py
+++ routes/details.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with photoframe.  If not, see <http://www.gnu.org/licenses/>.
 #
-
 import os
 import subprocess
 import logging
