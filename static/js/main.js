9387079fc557273d3f91890310c3248b893ee468
Dadr <tanschutz@bellsouth.net>
2021-06-17 13:26:08 -0400

    Finish Download
    debugged and finished download button

--- static/js/main.js
+++ static/js/main.js
@@ -346,33 +346,8 @@ $("#restore").click(function() {
   }
 });
 
-function downloadFile(file) {
-  // Create a link and set the URL using `createObjectURL`
-  const link = document.createElement("a");
-  link.style.display = "none";
-  link.href = URL.createObjectURL(file);
-  link.download = file.name;
-
-  // It needs to be added to the DOM so it can be clicked
-  document.body.appendChild(link);
-  link.click();
-
-  // To make this work on Firefox we need to wait
-  // a little while before removing it.
-  setTimeout(() => {
-    URL.revokeObjectURL(link.href);
-    link.parentNode.removeChild(link);
-  }, 0);
-}
-
 $("#dnldcfg").click(function() {
-  if (confirm("Download current settings?")) {
-    $.ajax({
-      url:"/maintenance/dnldcfg"
-    }).done(function (){
-      downloadFile("/tmp/settings.tar.gz");
-    });
-  }
+  window.location.assign("/maintenance/dnldcfg")
 });
 
 $("#upldcfg").click(function() {
