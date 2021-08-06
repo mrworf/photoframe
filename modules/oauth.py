d1c26469d95066547e7b1aad97303c1673a4663b
Henric Andersson <ha@sensenet.nu>
2021-08-05 22:39:09 -0700

    Fix backoff timer to actually back off

--- modules/oauth.py
+++ modules/oauth.py
@@ -89,7 +89,7 @@ class OAuth:
 				raise RequestInvalidToken
 			except:
 				logging.exception('Issues downloading')
-			time.sleep(tries / 10) # Back off 10, 20, ... depending on tries
+			time.sleep(tries * 10) # Back off 10, 20, ... depending on tries
 			tries += 1
 			logging.warning('Retrying again, attempt #%d', tries)
 
